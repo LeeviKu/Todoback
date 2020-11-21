e2b7f8142bb7772ab4dd6d32e7ad3a143800b92d
LeeviKu <leevi.kukkula@gmail.com>
2020-11-21 20:10:03 +0200

    Add express route to routes/tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const crud = require('../crudRepository.js')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  const response = await crud.findAll()
+  res.send(response)
+})
+
+module.exports = router

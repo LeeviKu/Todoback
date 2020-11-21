73c6498f711b40f37d640a4e173ff9a899f97ba7
LeeviKu <leevi.kukkula@gmail.com>
2020-11-21 20:09:14 +0200

    Add functions connect() and findAll()

--- crudRepository.js
+++ crudRepository.js
@@ -1,6 +1,25 @@
 const mysql = require('mysql')
-const config = require('../configs/config.js')
+const config = require('./config.js')
 
 class crudRepository {
-  
+  static connect () {
+    return new Promise((resolve, reject) => {
+      this.pool = mysql.createPool(config)
+      this.pool.getConnection((err, connection) => {
+        if (err) reject(err)
+        resolve(connection)
+      })
+    })
+  }
+
+  static findAll () {
+    return new Promise((resolve, reject) => {
+      this.pool.query('SELECT * FROM task', (err, result) => {
+        if (err) reject(err)
+        resolve(result)
+      })
+    })
+  }
 }
+
+module.exports = crudRepository

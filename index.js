b0b06710607acd36565e590004ba7033f4f0a738
LeeviKu <leevi.kukkula@gmail.com>
2020-11-21 20:10:42 +0200

    Add pool connection to index.js and tasks router

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const app = express();
-var cors = require("cors");
-app.use(cors());
+const express = require('express')
+const cors = require('cors')
+const tasks = require('./routes/tasks.js')
+const crud = require('./crudRepository.js')
 
-const port = process.env.PORT || 8080;
-const db = [{ name: "tiina" }, { name: "jack" }];
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/tasks', tasks)
 
-app.get("api/names", (req, res) => {
-  res.send(db);
-});
+const port = process.env.PORT || 8080
+const db = [{ name: 'tiina' }, { name: 'jack' }]
+
+app.get('/api/names', (req, res) => {
+  res.send(db)
+})
+
+crud.connect()
 
 const server = app.listen(port, () => {
-  console.log(`Listening on port ${server.address().port}`);
-}); 
+  console.log(`Listening on port ${server.address().port}`)
+})

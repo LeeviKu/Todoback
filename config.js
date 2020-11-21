d63ab2d2d2c29f3a3b38d7aeb6fdf749969f368b
LeeviKu <leevi.kukkula@gmail.com>
2020-11-21 20:50:55 +0200

    add config.js with environment variables

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+module.exports = {
+  connectionLimit: 10,
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.dbname
+}

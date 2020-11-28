78f0c889ef1d6925bdb44eaa09d40ecf3a05ee0b
LeeviKu <leevi.kukkula@gmail.com>
2020-11-28 08:48:59 +0200

    Add class queryParamHandler.
    queryParamHandler takes httpResponse, reject and pool as params.
    queryParamHandler has functions sort(sort), limit(limit) and
    offset(offset) that are used to handle query parameters.

--- database/api.js
+++ database/api.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const queryParamHandler = require("../queryParamHandler.js");
 const config = require("./config.js");
 
 class crudRepository {
@@ -14,35 +15,25 @@ class crudRepository {
 
   static findAllTasks(query, res) {
     return new Promise((resolve, reject) => {
+      const queryParams = { "orderby": "" ,"limit": "", "offset": ""}
+      this.paramHandler = new queryParamHandler(res, reject, this.pool)
+
+      if (query.limit) {
+        queryParams.limit = this.paramHandler.limit(query.limit)
+      }
+
+      if (query.offset) {
+        queryParams.offset = this.paramHandler.offset(query.offset, query.limit)
+      }
+
       if (query.sort) {
-        const orderBy = query.sort.slice(1);
-        let order = query.sort.slice(0, 1);
+        queryParams.orderby = this.paramHandler.sort(query.sort)
+      }
 
-        if (order === '-') {
-          order = 'DESC';
-        } else if (order === ' ') { // "+" === " "
-          order = 'ASC'
-        } else {
-          res.status(400)
-          res.send({"msg": "sort query should start with a - or +"})
-          reject(new Error('sort query should start with a - or +'))
-        }
-
-        const sortSet = new Set(["deadline", "name", "priority"])
-        if (!sortSet.has(orderBy)) {
-          res.status(400)
-          res.send({ "msg" : "You can only sort by deadline, name or priority" })
-        }
-        this.pool.query("SELECT * FROM task ORDER BY " + this.pool.escapeId(orderBy) + order, (err, result) => {
-          if (err) reject(err);
-          resolve(result);
-        });
-      } else {
-      this.pool.query("SELECT * FROM task", (err, result) => {
+      this.pool.query("SELECT * FROM task" + queryParams.orderby + queryParams.limit + queryParams.offset, (err, result) => {
         if (err) reject(err);
         resolve(result);
       });
-      }
     });
   }
 
